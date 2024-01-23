29ee02ae4beb7451d2094ad56da8eece2441badf
aiafaa <156507427+aiafaa@users.noreply.github.com>
2024-01-24 00:59:52 +1000

    Delete course directory

--- course/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Course(models.Model):
-    rate = models.FloatField()
-    date = models.DateTimeField(auto_now_add=True)
--- course/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import get_current_usd
-
-
-urlpatterns = [
-    path('get-current-usd/', get_current_usd)
-]
